2048696f8babd6e2c029bd8e36d1c7d7b6ee08f7
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-01-30 16:42:14 -0500

    Initial Bootstrap add

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+app.use(createBootstrap())
+app.mount('#app')
