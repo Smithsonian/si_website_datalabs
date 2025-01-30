dfbb7fa06f2bad2ffc0bad9d85973a9f9cdf2217
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-01-30 16:57:03 -0500

    Replace demo content with real content

---
+++ src/components/DataLabsIntro.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Data Labs</h1>
+  <p>
+    are technology-enabled instructional modules that, over 3–8 class periods, engage high school
+    students in authentic investigations using real data and online data visualization tools.
+    Developed with funding from the National Science Foundation, the Smithsonian Institution, and
+    NASA, these data-centric explorations incorporate exciting trans-disciplinary content
+    connections from across the Smithsonian.
+  </p>
+</template>
---
+++ src/components/DataLabsNavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BNavbar, BNavbarBrand } from 'bootstrap-vue-next'
+</script>
+
+<template>
+  <BNavbar>
+    <BNavbarBrand href="#"></BNavbarBrand>
+  </BNavbar>
+</template>
