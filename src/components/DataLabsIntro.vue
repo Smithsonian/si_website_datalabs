14c822d454b548248eb3ff25055fb7de954f1f57
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-02-03 17:30:41 -0500

    Home page first pass with assets

--- src/components/DataLabsIntro.vue
+++ src/components/DataLabsIntro.vue
@@ -1,10 +1,31 @@
+<script setup lang="ts">
+import { BCol, BContainer, BRow } from 'bootstrap-vue-next'
+</script>
+
 <template>
-  <h1>Data Labs</h1>
-  <p>
-    are technology-enabled instructional modules that, over 3–8 class periods, engage high school
-    students in authentic investigations using real data and online data visualization tools.
-    Developed with funding from the National Science Foundation, the Smithsonian Institution, and
-    NASA, these data-centric explorations incorporate exciting trans-disciplinary content
-    connections from across the Smithsonian.
-  </p>
+  <div class="text-light green-purple-lines-bg d-flex flex-column justify-content-end">
+    <BContainer>
+      <BRow>
+        <BCol cols="6">
+          <h1>Data Labs</h1>
+          <p>
+            are technology-enabled instructional modules that, over 3–8 class periods, engage high
+            school students in authentic investigations using real data and online data
+            visualization tools. Developed with funding from the National Science Foundation, the
+            Smithsonian Institution, and NASA, these data-centric explorations incorporate exciting
+            trans-disciplinary content connections from across the Smithsonian.
+          </p>
+        </BCol>
+      </BRow>
+    </BContainer>
+  </div>
 </template>
+
+<style>
+.green-purple-lines-bg {
+  background-image: url('/src/assets/green_purple_data_lines.jpg');
+  background-size: cover;
+  background-position: top 100% left 50%;
+  min-height: 450px;
+}
+</style>
--- src/components/DataLabsNavBar.vue
+++ src/components/DataLabsNavBar.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import { BNavbar, BNavbarBrand } from 'bootstrap-vue-next'
+import {
+  BCollapse,
+  BNavbar,
+  BNavbarBrand,
+  BNavbarNav,
+  BNavbarToggle,
+  BNavItem,
+} from 'bootstrap-vue-next'
 </script>
 
 <template>
-  <BNavbar>
-    <BNavbarBrand href="#"></BNavbarBrand>
+  <BNavbar v-b-color-mode="'dark'" variant="dark">
+    <BNavbarBrand href="#">
+      <img src="/src/assets/DataLabsLogo@2x.png" height="90" width="343" />
+    </BNavbarBrand>
+    <BNavbarToggle target="nav-collapse" />
+    <BCollapse id="nav-collapse" is-nav>
+      <BNavbarNav class="ms-auto">
+        <BNavItem href="#">DIY Planet Search</BNavItem>
+        <div class="vr bg-light"></div>
+        <BNavItem href="#">Spectrum Lab</BNavItem>
+      </BNavbarNav>
+    </BCollapse>
   </BNavbar>
 </template>
