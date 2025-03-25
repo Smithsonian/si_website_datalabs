f042d60134aa628e364dedde3e8246a1547666fd
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-03-25 13:03:29 -0400

    Make tiles links

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
-import DataLabsFooter from './components/DataLabsFooter.vue';
-import DataLabsIntro from './components/DataLabsIntro.vue';
-import DataLabsNavBar from './components/DataLabsNavBar.vue';
-
 useHead({ title: 'Smithsonian Datalabs' });
 </script>
+
 <template>
   <header>
     <DataLabsNavBar />
--- src/components/DataLabsTiles.vue
+++ src/components/DataLabsTiles.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { DIY_URL } from '@/constants';
 import { BCol, BContainer, BRow } from 'bootstrap-vue-next';
 </script>
 
@@ -7,41 +8,50 @@ import { BCol, BContainer, BRow } from 'bootstrap-vue-next';
     <BContainer>
       <BRow>
         <BCol cols="4">
-          <div class="card bg-dl-dark-purple text-light h-100">
-            <div class="card-img-top dl-tile-image-window">
-              <img
-                src="/src/assets/wasp-96b.png"
-                class="dl-tile-image"
-                alt="Exoplanet transiting a star"
-              />
+          <a :href="DIY_URL" class="text-decoration-none">
+            <div class="card dl-tile text-light h-100">
+              <div class="card-img-top dl-tile-image-window position-relative">
+                <img
+                  src="/src/assets/wasp-96b.png"
+                  class="dl-tile-image"
+                  alt="Exoplanet transiting a star"
+                />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">DIY Planet Search</h5>
+                <p class="card-text">
+                  New planets are being discovered on a regular basis. After new planets have been
+                  discovered the scientific community needs to follow up with further
+                  investigations. Help scientists better understand the movements of these planets
+                  around other stars by taking your own measurements of known exoplanets and
+                  contributing them to the DIY Planet Search Community. Using the DIY tools you’ll
+                  collect and analyze your own images of other star systems.
+                </p>
+              </div>
             </div>
-            <div class="card-body">
-              <h5 class="card-title">DIY Planet Search</h5>
-              <p class="card-text">
-                New planets are being discovered on a regular basis. After new planets have been
-                discovered the scientific community needs to follow up with further investigations.
-                Help scientists better understand the movements of these planets around other stars
-                by taking your own measurements of known exoplanets and contributing them to the DIY
-                Planet Search Community. Using the DIY tools you’ll collect and analyze your own
-                images of other star systems.
-              </p>
-            </div>
-          </div>
+          </a>
         </BCol>
         <BCol cols="4">
-          <div class="card bg-dl-dark-purple text-light h-100">
-            <img src="/src/assets/Filters.jpeg" class="card-img-top" alt="Colored glass filters" />
-            <div class="card-body">
-              <h5 class="card-title">Spectrum Lab</h5>
-              <p class="card-text">
-                The Spectrum Lab is an online data visualization tool and associated set of
-                investigations that supports students in learning about light, color, and the
-                electromagnetic spectrum by working with hands-on materials, models and simulations,
-                and authentic scientific spectral data. Implemented over 3 to 8 class periods,
-                Spectrum Lab activities promote students’ reasoning about light and color phenomena.
-              </p>
+          <a :href="DIY_URL" class="text-decoration-none">
+            <div class="card dl-tile text-light h-100">
+              <img
+                src="/src/assets/Filters.jpeg"
+                class="card-img-top"
+                alt="Colored glass filters"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Spectrum Lab</h5>
+                <p class="card-text">
+                  The Spectrum Lab is an online data visualization tool and associated set of
+                  investigations that supports students in learning about light, color, and the
+                  electromagnetic spectrum by working with hands-on materials, models and
+                  simulations, and authentic scientific spectral data. Implemented over 3 to 8 class
+                  periods, Spectrum Lab activities promote students’ reasoning about light and color
+                  phenomena.
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         </BCol>
       </BRow>
     </BContainer>
@@ -49,6 +59,14 @@ import { BCol, BContainer, BRow } from 'bootstrap-vue-next';
 </template>
 
 <style>
+.card.dl-tile {
+  background-color: var(--dl-dark-purple);
+  transition: background-color 0.15s ease-in-out;
+}
+.card.dl-tile:hover {
+  background-color: var(--dl-dark-purple-hover);
+}
+
 .dl-tile-image-window {
   overflow: hidden;
   height: 200px;
