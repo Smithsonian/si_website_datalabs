6b753abf2e33142531b5f99b1d2cf74e36eb25d7
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-10-17 16:31:43 -0400

    Tiles: switch order to match mock and nav

--- src/components/DataLabsTileList.vue
+++ src/components/DataLabsTileList.vue
@@ -3,6 +3,25 @@
     <BContainer>
       <BRow gutter-y="4">
         <DataLabsTile
+          :link-url="DIY_URL"
+          :img-src="diyImg"
+          img-alt="Exoplanet transiting a star"
+          card-title="DIY Planet Search"
+        >
+          <p>
+            An interactive data interface where students engage with the messiness of real data,
+            using the same analytical methods that professional scientists use to separate signal
+            from noise in their investigations. Through 6 core activities students:
+          </p>
+
+          <ul>
+            <li>Explore models of exoplanets</li>
+            <li>Collect real telescope data</li>
+            <li>Analyze their own images of other star systems</li>
+          </ul>
+          Implemented over 4 to 6 instructional hours.
+        </DataLabsTile>
+        <DataLabsTile
           :link-url="SPECLAB_URL"
           :img-src="speclabImg"
           img-alt="Colored glass filters"
@@ -21,25 +40,6 @@
             <li>Authentic spectral data from many disciplines</li>
           </ul>
           Implemented over 3 - 8 class periods.
-        </DataLabsTile>
-        <DataLabsTile
-          :link-url="DIY_URL"
-          :img-src="diyImg"
-          img-alt="Exoplanet transiting a star"
-          card-title="DIY Planet Search"
-        >
-          <p>
-            An interactive data interface where students engage with the messiness of real data,
-            using the same analytical methods that professional scientists use to separate signal
-            from noise in their investigations. Through 6 core activities students:
-          </p>
-
-          <ul>
-            <li>Explore models of exoplanets</li>
-            <li>Collect real telescope data</li>
-            <li>Analyze their own images of other star systems</li>
-          </ul>
-          Implemented over 4 to 6 instructional hours.
         </DataLabsTile>
       </BRow>
     </BContainer>
