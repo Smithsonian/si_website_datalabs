4b9ee3cdbdc2fdba4134d9f5a224da6f34e79081
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2026-01-22 14:25:51 -0500

    URLs: expect trailing slash in base URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // This checks DEV because the URLs won't make any sense on dev,
-// they only make sense on deployment
-export const DATA_LABS_BASE = import.meta.env.DEV
-  ? 'https://datalabs.cfa.harvard.edu'
-  : import.meta.env.BASE_URL;
+// they only make sense on deployment.
+// Remove the trailing slash
+const baseURL = import.meta.env.BASE_URL.slice(0, -1);
+export const DATA_LABS_BASE = import.meta.env.DEV ? 'https://datalabs.cfa.harvard.edu' : baseURL;
 export const DIY_URL = `${DATA_LABS_BASE}/diy/`;
 export const SPECLAB_URL = `${DATA_LABS_BASE}/spectrumlab/`;
